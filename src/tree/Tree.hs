3ba32931d4bfed621b4055e5729a5ec408d0937d
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-08-17 15:37:58 +0200

    safetree and toset

--- src/tree/Tree.hs
+++ src/tree/Tree.hs
@@ -3,6 +3,7 @@
 module Tree where
 import Data.List
 import Data.Maybe
+import Data.Set
 
 import Util
 
@@ -36,18 +37,29 @@ instance Applicative Tree where
 --  traverse g t@(Tree x c) = fmap (traverse g) c
   
 isleaf :: Tree a -> Bool
-isleaf (Tree _ children) = null children
+isleaf (Tree _ children) = Data.List.null children
 
 element :: Tree a -> a
 element (Tree n _) = n
 
-tree :: Eq a => Tree a -> a -> Maybe(Tree a)
-tree t x = Tree.find t (\(Tree y _) -> x == y)
+tree :: (Eq a, Show a) => Tree a -> a -> Tree a
+tree t x = case safetree t x of
+  Nothing -> error $ "The element "++(show x)++" is not part of tree "++(show t)
+  Just t' -> t'
 
+toset :: Ord a => Tree a -> Set a
+toset t = fromList $ foldMap pure t
+
+safetree :: Eq a => Tree a -> a -> Maybe(Tree a)
+safetree t x = Tree.find t (\(Tree y _) -> x == y)
+
+{-
+Find the first subtree in the given tree (first argument) whose root matches the predicate (second argument).
+-}
 find :: Tree a -> (Tree a -> Bool) -> Maybe(Tree a)
 find x@(Tree _ children) predicate = case predicate x of
   True -> Just x
-  False -> safehead $ catMaybes $ map (\t -> Tree.find t predicate) children
+  False -> safehead $ catMaybes $ fmap (\t -> Tree.find t predicate) children
 
 parent :: Eq a => Tree a -> Tree a -> Maybe(Tree a)
 parent root t = Tree.find root (\(Tree _ children) -> elem t children)
