289166a62560242bac03bd8696c8a6db43657ef4
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-08-19 17:16:26 +0200

    picosat call

--- src/propositions/SAT.hs
+++ src/propositions/SAT.hs
@@ -3,8 +3,16 @@ module SAT where
 import Propositions
 import Picosat
 
-sat :: PropositionalFormula a -> Bool
-sat p = False
+toPicosatFormat :: PropositionalFormula a -> [[Int]]
+toPicosatFormat p = [[1]] --(map toDNFClauseList $ toCNFClauseList $ lazyToCNF p)
 
-taut :: PropositionalFormula a -> Bool
-taut p = not $ sat $ PNot p
+sat :: PropositionalFormula a -> IO Bool
+sat p = do
+    res <- solve $ toPicosatFormat p
+    case res of
+        Solution _ -> return True
+        Unsatisfiable -> return False
+        Unknown -> return False
+
+taut :: PropositionalFormula a -> IO Bool
+taut p = sat (PNot p) >>= \s -> return $ not s
