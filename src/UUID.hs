a652732db505297ff78bff3f029407d93f10911b
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-08-20 14:06:29 +0200

    extracted uuid to own utility

---
+++ src/UUID.hs
@@ -0,0 +1,14 @@
+module UUID where
+
+import Control.Monad.State
+
+type UUID = Int
+
+next :: () -> State UUID ()
+next x = do
+    num <- get
+    put (num + 1)
+    return x
+
+toInt :: UUID -> Int
+toInt = id
--- src/tree/AST.hs
+++ src/tree/AST.hs
@@ -2,10 +2,10 @@
 
 module AST where
 
+import UUID
 import Tree
 import Control.Monad.State
 
-type UUID = Int
 
 data NodeType = Plain | Constituent | Legator deriving (Show, Eq)
 data Node a = Node {value::a, ntype::NodeType, uuid::UUID} deriving (Show, Eq, Functor) --, version::Int
@@ -18,9 +18,12 @@ instance (Eq a) => Ord (Node a) where
   v <= w = (uuid v) <= (uuid w)
 
 newNode :: a -> NodeType -> State UUID (Node a)
-newNode a t = do num <- get
-                 put (num + 1)
-                 return Node {value = a, ntype = t, uuid = num} --, version = 0
+newNode a t = do
+  next ()
+  id <- get
+  return Node {value = a, ntype = t, uuid = id} --, version = 0
+
+  
 
 -- increaseVersion :: Int -> Node a -> Node a
 -- increaseVersion x n = Node {version = x + version n, value = value n, ntype = ntype n, uuid = uuid n}
