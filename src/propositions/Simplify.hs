88ad0590cd81682f4d28f4f777162ed1d1561df1
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-09-01 17:38:25 +0200

    prototype for simplifying a formula given an axiom

---
+++ src/propositions/Simplify.hs
@@ -0,0 +1,8 @@
+﻿module Simplify where
+
+import Propositions
+import SAT
+
+removeRedundancy :: (Ord a, Show a) => PropositionalFormula a -> PropositionalFormula a -> PropositionalFormula a
+removeRedundancy axiom (PAnd cs) = simplify $ PAnd $ foldr (\a b -> if (taut $ pimplies (PAnd $ a:b) axiom) then b else a:b) [] $ (removeRedundancy axiom) <$> cs
+removeRedundancy axiom x = simplify $ if taut $ pimplies axiom x then PTrue else x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,37 +1,57 @@
 import Propositions
 import SAT
 import Util
+import Simplify
 
-testImpl :: PropositionalFormula String
-testImpl = pimplies (PAnd [PVariable "A", PVariable "B"]) (PVariable "C")
+-- testImpl :: PropositionalFormula String
+-- testImpl = pimplies (PAnd [PVariable "A", PVariable "B"]) (PVariable "C")
 
-testDNF :: PropositionalFormula String
-testDNF = POr [PAnd [PVariable "A", PVariable "B"], PAnd [PVariable "C", PNot (POr [PVariable "A", PVariable "B"])]]
+-- testDNF :: PropositionalFormula String
+-- testDNF = POr [PAnd [PVariable "A", PVariable "B"], PAnd [PVariable "C", PNot (POr [PVariable "A", PVariable "B"])]]
 
-testNot :: PropositionalFormula String
-testNot = PNot testImpl
+-- testNot :: PropositionalFormula String
+-- testNot = PNot testImpl
 
-testCNF :: PropositionalFormula String
-testCNF = PAnd [POr [PVariable "A", PVariable "B"], POr [PNot (PVariable "A"), PVariable "D"]]
+-- testCNF :: PropositionalFormula String
+-- testCNF = PAnd [POr [PVariable "A", PVariable "B"], POr [PNot (PVariable "A"), PVariable "D"]]
 
-testTrue :: PropositionalFormula String
-testTrue = PAnd [PTrue, POr [PFalse, pimplies PFalse PTrue]]
+-- testTrue :: PropositionalFormula String
+-- testTrue = PAnd [PTrue, POr [PFalse, pimplies PFalse PTrue]]
 
-testFalse :: PropositionalFormula String
-testFalse = PAnd [PAnd [PFalse, PTrue], POr [PFalse, pimplies PFalse PTrue]]
+-- testFalse :: PropositionalFormula String
+-- testFalse = PAnd [PAnd [PFalse, PTrue], POr [PFalse, pimplies PFalse PTrue]]
 
-{-Test CNF conversion-}
-main = putStrLn $ 
-    foldr (\a b -> a++"\n"++b) "" $
-    map (\a -> let c = toCNF a in
-        "   Formula: "++(show a)++
-        "\n  -> isCNF: "++(show $ isCNF a)++
-        "\n       CNF: "++(show $ c)++
-        "\n  -> isCNF: "++(show $ isCNF $ c)++
-        "\nclausified: "++(show $ clausifyCNF (\s -> "not "++s) (\() -> "False") c)++
-        "\nin picosat: "++(show $ toIntCNF a)++
-        "\n       sat: "++(show $ sat a)++
-        "\n=====\n")
-    [testImpl, testDNF, testNot, testCNF, testTrue, testFalse]
+-- {-Test CNF conversion-}
+-- main :: IO ()
+-- main = putStrLn $ 
+--     foldr (\a b -> a++"\n"++b) "" $
+--     map (\a -> let c = toCNF a in
+--         "   Formula: "++(show a)++
+--         "\n  -> isCNF: "++(show $ isCNF a)++
+--         "\n       CNF: "++(show $ c)++
+--         "\n  -> isCNF: "++(show $ isCNF $ c)++
+--         "\nclausified: "++(show $ clausifyCNF (\s -> "not "++s) (\() -> "False") c)++
+--         "\nin picosat: "++(show $ toIntCNF a)++
+--         "\n       sat: "++(show $ sat a)++
+--         "\n=====\n")
+--     [testImpl, testDNF, testNot, testCNF, testTrue, testFalse]
 
--- main = putStrLn . show $ getRange 2 4 ['a'..'f']
+-- -- main = putStrLn . show $ getRange 2 4 ['a'..'f']
+
+testCases :: [(PropositionalFormula [Char], PropositionalFormula [Char])]
+testCases = [
+    (PAnd [PVariable "A", PVariable "B"], PVariable "A"),
+    (PVariable "A", PAnd [PVariable "A", PVariable "B"]),
+    (PVariable "A", PAnd [PVariable "B", PVariable "A"]),
+    (POr [PVariable "A", PVariable "B"], POr [PVariable "B", PVariable "A"]),
+    (POr [PVariable "A", PVariable "B"], PAnd [POr [PVariable "B", PVariable "A"], PVariable "X"]),
+    (POr [PVariable "A", PVariable "B"], PAnd [PVariable "X", POr [PVariable "B", PVariable "A"]])
+    ]
+
+main :: IO ()
+main = putStrLn $
+    foldr (\a b -> a++"\n"++b) mempty $
+    map (\(parent, formula) -> concat [
+        "             axiom = ", show parent, "\n",
+        "           formula = ", show formula, "\n",
+        "simplified formula = ", show $ removeRedundancy parent formula, "\n"]) testCases
