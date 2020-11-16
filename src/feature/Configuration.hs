c5e49421a6e4d368a1701a773563d815289378b7
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-11-16 17:58:17 +0100

    babysteps towards target variant selection

---
+++ src/feature/Configuration.hs
@@ -0,0 +1,15 @@
+﻿module Configuration where
+
+import Data.Map
+import PropositionalFormula
+
+type Configuration a = Map a Bool
+
+satisfies :: Configuration a -> PropositionalFormula a -> Bool
+satisfies config = eval (config!)
+
+isTotal :: Set a -> Configuration a -> Bool
+isTotal variables config = (keysSet config) isSubsetOf variables
+
+isPartial :: Set a -> Configuration a -> Bool
+isPartial = not.isTotal
---
+++ src/variant/Variant.hs
@@ -0,0 +1,33 @@
+﻿module Variant where
+
+import Configuration
+import FeatureTrace
+import FeatureTraceRecording
+import AST
+import Edits
+
+data Variant g a = Variant {
+    config :: Configuration,
+    trace :: FeatureTrace g a,
+    artefacts :: AST g a,
+    history :: RecordedEditScript g a,
+    initialTrace :: FeatureTrace g a,
+    initialArtefacts :: AST g a
+}
+
+instance Eq a => Eq (Variant g a) where
+    v == w = (config v) == (config w)
+
+createVariant :: Configuration -> FeatureTrace g a -> AST g a -> Variant g a
+createVariant c t a = Variant {
+    config = c,
+    trace = t,
+    artefacts = a,
+    history = [],
+    initialTrace = t,
+    initialArtefacts = a
+}
+
+evolve :: RecordedEditScript g a -> Variant g a -> Variant g a
+evolve edits variant = variant { trace = newTrace, artefacts = newTree, history = (history variant)++edits}
+    where (newTrace, newTree) = featureTraceRecording defaultFeatureTraceRecording (trace variant) (artefacts variant) edits
