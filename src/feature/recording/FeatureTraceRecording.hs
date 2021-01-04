f4a7eda1bcaaa62c7aac09d6c261bbd4e0bbd73e
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-01-04 16:11:07 +0100

    small improvements on documentation

--- src/feature/recording/FeatureTraceRecording.hs
+++ src/feature/recording/FeatureTraceRecording.hs
@@ -16,21 +16,21 @@ type History g a = [RecordedEdit g a]
 
 {-
 We denote the state of a software system at any point in time as a Version.
-We describe the a version by the software artefacts in terms of an AST and the according feature traces we have at that version.
+We describe a version by the software artefacts in terms of an AST and the according feature traces we have at that version.
 -}
 type Version g a = (FeatureTrace g a, AST g a)
 
 {-
 Type for the recording functions for individual edit types.
 In the paper, these edits are referred to as R_ins, R_del, R_mov, and R_up.
-Instead of just passing the delta, we give the entire edit as input to the function from which the delta can be calculated.
+Instead of passing the delta, we give the entire edit as input to the function from which the delta can be calculated.
 -}
 type RecordingFunction g a = RecordedEdit g a -> Version g a -> FeatureTrace g a
 
 {-
 Abstraction over Algorithm 1 in the paper.
 For different use cases and to prevent future errors, feature trace recording is configurable.
-In a framework-like manner, it gives us an implementation for a recording function.
+In a framework-like manner, it gives us an implementation for a recording function identified by edit types.
 -}
 type FeatureTraceRecording g a = EditType -> RecordingFunction g a
 
